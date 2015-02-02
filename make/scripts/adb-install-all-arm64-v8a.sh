58bd5a07aca65a9763dd51ab070d9945df87a7a4
Sven Gothel <sgothel@jausoft.com>
2015-02-02 01:03:43 +0100

    Bug 1122 - Add ARM 64bit build support, add Android ARM 64 bit build (build/install scripts only)

---
+++ make/scripts/adb-install-all-arm64-v8a.sh
@@ -0,0 +1,4 @@
+#adb $* install ../../gluegen/build-android-aarch64/jogamp-android-launcher.apk
+#adb $* install ../../gluegen/build-android-aarch64/gluegen-rt-android-arm64-v8a.apk
+adb $* install ../build-android-aarch64/jar/jocl-android-arm64-v8a.apk
+adb $* install ../build-android-aarch64/jar/jocl-test-android.apk
