b81b9e14f24de0cbc66ab500afe0f8db7a058b96
Sven Gothel <sgothel@jausoft.com>
2015-02-03 00:26:33 +0100

    Adapt APK install scripts for harmonized os.and.arch names, see GlueGen commit 3b43a223253176731567c6d8b7a67c9a6110782b

---
+++ make/scripts/adb-install-all-aarch64.sh
@@ -0,0 +1,4 @@
+#adb $* install ../../gluegen/build-android-aarch64/jogamp-android-launcher.apk
+#adb $* install ../../gluegen/build-android-aarch64/gluegen-rt-android-aarch64.apk
+adb $* install ../build-android-aarch64/jar/jocl-android-aarch64.apk
+adb $* install ../build-android-aarch64/jar/jocl-test-android.apk
